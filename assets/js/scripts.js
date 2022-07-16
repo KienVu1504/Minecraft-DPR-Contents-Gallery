64c71b34b2a6d447abd952c3f14bc8e46e130237
Kien Vu <kvstudio.154@gmail.com>
2022-07-16 12:28:51 +0700

    redesign content list

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -36,24 +36,91 @@ function scrollFunction() {
     }
 }
 
-$(document).ready(function () {
-    var mContainer = $("#mnsry_container"),
-        filterButton = $(".button");
-        params = {
-            itemSelector: ".item",
-            filtersGroupSelector:".filters"
-        };
+function addClassAll(el, cls){
+	for (var i = 0; i < el.length; ++i){
+		if (!el[i].className.match('(?:^|\\s)'+cls+'(?!\\S)')){
+			el[i].className += ' '+cls; 
+		} 
+	}
+}
 
-    $(window).load(function() { 
-        // Do mansonry with filtering 
-        mContainer.multipleFilterMasonry(params);
+function delClassAll(el, cls){
+	for (var i = 0; i < el.length; ++i){
+		el[i].className = el[i].className.replace(
+			new RegExp('(?:^|\\s)'+cls+'(?!\\S)'),''
+		);
+	}
+}
 
-        // Show articles with fadein
-        mContainer.find(".item").animate({"opacity":1}, 1200);
+function contentFilter(filterID, filterType){  
+	var id = filterID;
+	document.querySelector(id+' .filter-categories').onclick = function(evt) { 
+		evt = evt || window.event;
 
-        // Change the filtering button(label) status 
-        filterButton.find("input").change(function(){
-            $(this).parent().toggleClass("active");
-        });
-    });
-});
+		var elem = evt.target || evt.srcElement, 
+		    wrap = document.querySelectorAll(id+' .filter-wrap'),
+		    items = document.querySelectorAll(id+' .filter-item'),
+		    inputs = document.querySelectorAll(id+' .filter-input'),
+		    filters = [],
+		    noitem = document.querySelectorAll(id+' .filter-no-item'),
+		    type = filterType;
+
+		if (elem.className.match('(?:^|\\s)filter-all(?!\\S)')) { // #filter-all
+			for (var i = 1; i < inputs.length; ++i) { // loop through inputs but ignore [0] - #filter-all
+				inputs[i].checked = false; // uncheck all other inputs
+			}
+
+			setTimeout(function() { 
+				delClassAll(items, 'selected');  
+				delClassAll(wrap, 'filtered-'+type); 
+				delClassAll(noitem, 'filter-no-item-active');
+			}, 500); 
+		} else { // another filter is checked
+			inputs[0].checked = false; // uncheck #filter-all
+			for (var i = 1; i < inputs.length; ++i) { // loop through inputs but ignore [0] - #filter-all
+				if (inputs[i].checked) { 
+					filters.push(inputs[i].id); 
+				} // add checked inputs to filters array 
+			}
+
+			setTimeout(function() { 
+				delClassAll(items, 'selected'); 
+				addClassAll(wrap, 'filtered-'+type);  
+
+				if (type == 'inclusive') {
+					if (filters.length > 0) {
+						addClassAll(document.querySelectorAll(id+' .filter-item.'+filters.join('.')), 'selected');
+					} // build css selector from filters array
+					// document.querySelectorAll(id+' .selected').length == 0 ? addClassAll(noitem, 'filter-no-item-active') : delClassAll(noitem, 'filter-no-item-active'); 
+
+					if(document.querySelectorAll(id+' .selected').length == 0){
+						addClassAll(noitem, 'filter-no-item-active')
+						addClassAll(wrap, 'filter-wrap-1')
+					} else {
+						delClassAll(noitem, 'filter-no-item-active');
+						delClassAll(wrap, 'filter-wrap-1');
+						addClassAll(wrap, 'filter-wrap')
+					}
+				} 
+
+				var checkCount = 0;  
+
+				for (var i = 0; i < inputs.length; ++i) {
+					checkCount += inputs[i].checked ? 1 : 0; 
+				}
+
+				if (checkCount == 0) {
+					inputs[0].checked = true;
+				}
+
+				if (inputs[0].checked) {
+					delClassAll(wrap, 'filtered-'+type);
+					delClassAll(noitem, 'filter-no-item-active');
+					delClassAll(wrap, 'filter-wrap-1');
+				}
+			}, 500); 
+		}
+	}
+}
+
+contentFilter('#filter_categories_id', 'inclusive');
